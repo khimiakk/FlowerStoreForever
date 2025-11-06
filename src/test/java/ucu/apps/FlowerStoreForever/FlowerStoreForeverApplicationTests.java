7cec740ba5125254913b3cce2b81e4203fc81692
khimiakk <mvkh2007@gmail.com>
2025-11-06 21:43:52 +0200

    fixes & additions

--- src/test/java/ucu/apps/FlowerStoreForever/FlowerStoreForeverApplicationTests.java
+++ src/test/java/ucu/apps/FlowerStoreForever/FlowerStoreForeverApplicationTests.java
@@ -2,8 +2,10 @@ package ucu.apps.flowerstoreforever;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FlowerStoreForeverApplicationTests {
 
     @Test
